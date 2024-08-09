7c9995b2acc0e876e95940e72fdf6e534f05aebb
milmilkat <mldktb@gmail.com>
2024-08-10 00:59:15 +1200

    updated

--- app.js
+++ app.js
@@ -1,19 +1,21 @@
 var express = require('express');
 var app = express();
 
-const counter1 = 50;
-const counter2 = 5;
+let counter1 = 50;
+let counter2 = 5;
 
 
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.send(`Counter1: ${counter1} \r\n Counter2: ${counter2}`);
 });
 
 app.get('/counter1', function (req, res) {
+    counter1--;
     res.send(counter1.toString());
 });
 
 app.get('/counter2', function (req, res) {
+    counter2--;
     res.send(counter2.toString());
 });
 
